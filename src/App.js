00255a7bdd5792ea7a38ddac541d96644159b85f
孙中强 <sunzhongqiang@126.com>
2018-10-10 14:58:10 +0800

    完成框架的部分搭建

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Layout, Menu,Icon } from 'antd';
 import './App.css';
+import Project from './component/Project';
+import Template from './component/Template';
+import Log from './component/Log';
+
+const { Header, Sider, Content } = Layout;
+const MenuItemGroup = Menu.ItemGroup;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router className="App">
+          <Layout>
+              <Header className="title">
+                  <div className="logo">代码助理</div>
+                  <Menu
+                      theme="dark"
+                      mode="horizontal"
+                      defaultSelectedKeys={['2']}
+                      style={{ lineHeight: '9vh',width: '300px' }}
+                  >
+                      <Menu.Item key="1"> <Link to="/">项目列表</Link></Menu.Item>
+                      <Menu.Item key="2"><Link to="/template">模板列表</Link></Menu.Item>
+                      <Menu.Item key="3"><Link to="/log">生成日志</Link></Menu.Item>
+                  </Menu>
+              </Header>
+              <Content className="content">
+                  <Route exact path="/" component={Project} />
+                  <Route path="/template" component={Template} />
+                  <Route path="/log" component={Log} />
+              </Content>
+          </Layout>
+      </Router>
     );
   }
 }
